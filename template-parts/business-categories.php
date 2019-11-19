d7beada27a234a1073c3bcd98c86341609943ab6
hermie <hermiebarit@gmail.com>
2019-11-19 19:33:35 +0800

    Fixing the height of the icons in business directory

--- template-parts/business-categories.php
+++ template-parts/business-categories.php
@@ -11,7 +11,7 @@
         <div class="cat">
             <a href="<?php echo $category['url']; ?>">
                 <div class="icon">
-                    <img src="<?php echo $category['icon']; ?>" alt="<?php echo $category['name']; ?>">
+                    <img src="<?php echo $category['icon']; ?>" alt="<?php echo $category['name']; ?>" class="business_category_icon">
                 </div>
                 <h2><?php echo $category['name']; ?></h2>
             </a>
