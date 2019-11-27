4261c6cc04f3277363eb0935e6b11dbf54fa595f
hermie <hermiebarit@gmail.com>
2019-11-27 17:41:24 +0800

    Fixing the brown bar in homepage

--- template-parts/subscribe-bar.php
+++ template-parts/subscribe-bar.php
@@ -1,5 +1,8 @@
 <div class="subscribe-wrap">
 	<div class="subscribe">
-		<?php echo ($subscribe_text) ? $subscribe_text : '';  ?> <a href="<?php echo ($subscribe_link) ? $subscribe_link : '';  ?>" class="subscribe_btn"><?php echo ($subscribe_button_name) ? $subscribe_button_name : '';  ?></a>
+		<?php echo ($subscribe_text) ? $subscribe_text : '';  ?> 
+        <?php if($subscribe_button_name): ?>
+        <a href="<?php echo ($subscribe_link) ? $subscribe_link : '';  ?>" class="subscribe_btn"><?php echo ($subscribe_button_name) ? $subscribe_button_name : '';  ?></a>
+        <?php endif; ?>
 	</div>
 </div>
