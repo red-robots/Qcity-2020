72f48b294dedadaa844d31782e1b438ddae04f32
hermie <hermiebarit@gmail.com>
2019-11-29 16:58:16 +0800

    Sticky on left side and recent posts on right side in homepage

--- template-parts/hero.php
+++ template-parts/hero.php
@@ -6,11 +6,11 @@ $i = 0;
 $postIDs = array();
 $wp_query = new WP_Query();
 $wp_query->query(array(
-	'post_type'=>'post',
-	'posts_per_page' => 3,
-	'post_status'  => 'publish',
-	'post__in' => get_option('sticky_posts'),
-	'ignore_sticky_posts' => 1
+	'post_type'				=>'post',
+	'posts_per_page' 		=> 1,
+	'post_status'  			=> 'publish',
+	'post__in' 				=> get_option('sticky_posts'),
+	'ignore_sticky_posts' 	=> 1
 ));
 if ($wp_query->have_posts()) : ?>
 <section class="stickies">
@@ -25,7 +25,7 @@ if ($wp_query->have_posts()) : ?>
 		//var_dump($img);
 		
 		// First Post
-		if( $i == 1 ) {
+		
 	?>
 	
 		<article class="big-post">
@@ -52,11 +52,38 @@ if ($wp_query->have_posts()) : ?>
 			</div>
 		</article>		
 	</div>
+	<?php endwhile;
+
+	endif;  wp_reset_postdata(); ?>
+
+	<?php
+		//$category = get_term_by( 'slug', 'offers-invites');
+
+		$slug = 'offers-invites';
+		$cat = get_category_by_slug($slug); 
+		$catID = $cat->term_id;
+
+		//var_dump($catID);
+
+		$args = array(
+			'post_type'			=>'post',
+			'posts_per_page' 	=> 2,
+			'post_status'  		=> 'publish',
+			'post__not_in' 		=> $postIDs,
+			'category__not_in' 	=> $catID ,
+		);
+
+		$recent_query = new WP_Query( $args ); 
+
+		if( $recent_query->have_posts() ):
+	?>
+
 	<div class="right">
-		<?php } else { ?>
+		
 		<?php 
 			$i++;
-			//$img = get_field('story_image');
+			while ($recent_query->have_posts()) :  $recent_query->the_post();
+				$img = get_field('story_image');
 
 			?>
 			<article class="story-block">
@@ -79,13 +106,11 @@ if ($wp_query->have_posts()) : ?>
 					
 				</div>
 				<div class="by">
-					By: <?php the_author(); ?> | <?php the_date(); ?>
+					By: <?php the_author(); ?> | <?php echo get_the_date(); ?>
 				</div>
 				<div class="article-link"><a href="<?php the_permalink(); ?>"></a></div>
 			</article>
-		<?php 
-			//include( locate_template('template-parts/story-block.php', false, false) );
-		 } endwhile; ?>
+		<?php endwhile; ?>
 	</div>
 	</section>
 <?php 
