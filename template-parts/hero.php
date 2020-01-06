5c241bd27ab6aa0952acedb51bc532578cc4a73c
hermie <hermiebarit@gmail.com>
2020-01-07 07:58:55 +0800

    Mobile ads

--- template-parts/hero.php
+++ template-parts/hero.php
@@ -121,7 +121,7 @@ if ($wp_query->have_posts()) : ?>
                     }
 
                  if($i == 1){
-                 	echo '<div class="mobile-version bottom-20 right-ads">'. get_ads_script('right-rail') . '</div>';
+                 	echo '<div class="mobile-version bottom-20 mobile-ads">'. get_ads_script('right-rail') . '</div>';
                  }   
 
 		 endwhile; 
