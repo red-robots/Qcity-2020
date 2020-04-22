322cba68894c929eca13e28ff7ce32b1ebfc09ac
hermie <hermiebarit@gmail.com>
2020-04-22 19:50:29 +0800

    Changing recipient

--- inc/custom-comment.php
+++ inc/custom-comment.php
@@ -64,7 +64,7 @@ function save_comment_meta_data( $comment_id ) {
         $message .= '<p>City: '. $city .'</p>';
         $message .= '<p>Daytime Phone: '. $phone .'</p>';
 
-        $headers = 'From: QCity Metro<'. $email_recipient .'>;' . "\r\n";
+        $headers = 'From: QCity Metro<'. $email_recipient .'>;
 
         add_filter( 'wp_mail_content_type', create_function( '', 'return "text/html";' ) );
         wp_mail( $email_recipient, 'New Comment from ' . $comment->comment_author, $message, $headers );
