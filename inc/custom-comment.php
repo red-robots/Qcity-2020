0159b02993a75c00a6cef4dab86ac531b3eb811f
hermie <hermiebarit@gmail.com>
2020-04-22 22:54:50 +0800

    Applying header from in email

--- inc/custom-comment.php
+++ inc/custom-comment.php
@@ -64,7 +64,10 @@ function save_comment_meta_data( $comment_id ) {
         $message .= '<p>City: '. $city .'</p>';
         $message .= '<p>Daytime Phone: '. $phone .'</p>';
 
-        $headers = 'From: QCity Metro<'. $email_recipient .'>;';
+        //$headers = 'From: QCity Metro<'. $email_recipient .'>;' . "\r\n";
+        $headers = array(
+            'From: QCity Metro<'. $email_recipient .'>;'            
+        );
 
         add_filter( 'wp_mail_content_type', create_function( '', 'return "text/html";' ) );
         wp_mail( $email_recipient, 'New Comment from ' . $comment->comment_author, $message, $headers );
